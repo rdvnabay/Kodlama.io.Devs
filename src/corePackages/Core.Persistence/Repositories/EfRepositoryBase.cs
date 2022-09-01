8c136e2e9037719dfdc0b38637e55687184da187
Ridvan <ridvanabay@gmail.com>
2022-09-01 22:24:23 +0300

    Core Persistence: Add base repository

---
+++ src/corePackages/Core.Persistence/Repositories/EfRepositoryBase.cs
@@ -0,0 +1,45 @@
+﻿using Core.Domain.Entities;
+using Core.Persistence.Paging;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Core.Persistence.Repositories;
+public class EfRepositoryBase<TContext, TEntity> : IAsyncRepository<TEntity>
+    where TContext : DbContext
+    where TEntity : class, IEntity
+{
+    protected TContext Context { get; }
+    public EfRepositoryBase(TContext context)
+    {
+        Context = context;
+    }
+
+    public async Task<IPaginate<TEntity>> GetListAsync(int index = 0, int size = 10, CancellationToken cancellationToken = default)
+    {
+        IQueryable<TEntity> queryable = Context.Set<TEntity>();
+        return await queryable.ToPaginateAsync(index, size, 0, cancellationToken);
+    }
+
+    public async Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> predicate)
+        => await Context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+
+    public async Task<TEntity> AddAsync(TEntity entity)
+    {
+        Context.Entry(entity).State = EntityState.Added;
+        await Context.SaveChangesAsync();
+        return entity;
+    }
+    public async Task<TEntity> UpdateAsync(TEntity entity)
+    {
+        Context.Entry(entity).State = EntityState.Modified;
+        await Context.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<TEntity> DeleteAsync(TEntity entity)
+    {
+        Context.Entry(entity).State = EntityState.Deleted;
+        await Context.SaveChangesAsync();
+        return entity;
+    }
+}
---
+++ src/corePackages/Core.Persistence/Repositories/IAsyncRepository.cs
@@ -0,0 +1,12 @@
+﻿using Core.Persistence.Paging;
+using System.Linq.Expressions;
+
+namespace Core.Persistence.Repositories;
+public interface IAsyncRepository<TEntity>
+{
+    Task<IPaginate<TEntity>> GetListAsync(int index = 0, int size = 10, CancellationToken cancellationToken = default);
+    Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> predicate);
+    Task<TEntity> AddAsync(TEntity entity);
+    Task<TEntity> UpdateAsync(TEntity entity);
+    Task<TEntity> DeleteAsync(TEntity entity);
+}
