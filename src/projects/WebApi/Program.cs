7e2a10cd984b28fce76c6f5973e02511c2bfcab3
Ridvan <ridvanabay@gmail.com>
2022-09-02 18:37:31 +0300

    WebApi: add Base controller

--- src/projects/WebApi/Program.cs
+++ src/projects/WebApi/Program.cs
@@ -1,11 +1,14 @@
+using Application;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddPersistenceServices(builder.Configuration);
+builder.Services.AddApplicationServices();
 
 var app = builder.Build();
 
@@ -17,7 +20,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
