da605d771775476d3ddd36215c0f008cb6de7a13
Ridvan <ridvanabay@gmail.com>
2022-09-02 21:53:02 +0300

    #3 Add validation rule: Programming language not empty or not null

---
+++ src/projects/Application/Features/ProgrammingLanguages/Commands/CreateProgramminLanguage/CreateProgramminLanguageCommandValidator.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Features.ProgrammingLanguages.Commands.CreateProgramminLanguage;
+
+public class CreateProgramminLanguageCommandValidator : AbstractValidator<CreateProgrammingLanguageCommand>
+{
+    public CreateProgramminLanguageCommandValidator()
+    {
+        RuleFor(pl => pl.ProgrammingLanguage.Name).NotEmpty().NotNull();
+    }
+}
---
+++ src/projects/Application/Features/ProgrammingLanguages/Commands/UpdateProgrammingLanguage/UpdateProgrammingLanguageCommandValidator.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Features.ProgrammingLanguages.Commands.UpdateProgrammingLanguage;
+
+public class UpdateProgrammingLanguageCommandValidator : AbstractValidator<UpdateProgrammingLanguageCommand>
+{
+    public UpdateProgrammingLanguageCommandValidator()
+    {
+        RuleFor(pl => pl.ProgrammingLanguage.Name).NotEmpty().NotNull();
+    }
+}
