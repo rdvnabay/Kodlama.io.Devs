75397370c4e7d418bf719cac3679e690b9c95b49
Ridvan <ridvanabay@gmail.com>
2022-09-08 20:32:57 +0300

    #4 Persistence | migration Add-TechnologyTable

--- src/projects/Persistence/Migrations/20220908173029_Add-TechnologyTable.Designer.cs
+++ src/projects/Persistence/Migrations/20220908173029_Add-TechnologyTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence.Contexts;
 
@@ -10,9 +11,10 @@ using Persistence.Contexts;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220908173029_Add-TechnologyTable")]
+    partial class AddTechnologyTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
