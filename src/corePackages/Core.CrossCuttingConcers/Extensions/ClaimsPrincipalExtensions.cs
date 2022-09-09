e96beb6a9e407ad54492df53f7d9e6be55e7967f
Ridvan <ridvanabay@gmail.com>
2022-09-09 22:37:09 +0300

    #5 Core Extensions | add Claim and ClaimsPrincipal extensions

---
+++ src/corePackages/Core.CrossCuttingConcers/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+namespace Core.CrossCuttingConcers.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static List<string>? Claims(this ClaimsPrincipal claimsPrincipal, string claimType)
+        => claimsPrincipal?.FindAll(claimType)?.Select(c => c.Value).ToList();
+
+    public static List<string>? ClaimRoles(this ClaimsPrincipal claimsPrincipal)
+        => claimsPrincipal?.Claims(ClaimTypes.Role);
+
+    public static int GetUserId(this ClaimsPrincipal claimsPrincipal)
+        => Convert.ToInt32(claimsPrincipal?.Claims(ClaimTypes.NameIdentifier)?.FirstOrDefault());
+}
