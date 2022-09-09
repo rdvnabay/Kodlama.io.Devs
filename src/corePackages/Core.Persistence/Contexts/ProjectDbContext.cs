7fa768c6c75e3990b4d4f5aa7c69a11b3aaab65e
Ridvan <ridvanabay@gmail.com>
2022-09-09 22:41:09 +0300

    Core Persistence | add ProjectDbContext

---
+++ src/corePackages/Core.Persistence/Contexts/ProjectDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+namespace Core.Persistence.Contexts;
+
+public class ProjectDbContext:DbContext
+{
+    protected IConfiguration Configuration { get; set; }
+
+    public ProjectDbContext(DbContextOptions<ProjectDbContext> options, IConfiguration configuration) : base(options)
+    {
+        Configuration = configuration;
+    }
+
+    public ProjectDbContext(DbContextOptions options, IConfiguration configuration) : base(options)
+    {
+        Configuration = configuration;
+    }
+
+    public DbSet<User> User { get; set; }
+    public DbSet<OperationClaim> OperationClaims { get; set; }
+    public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+     => modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+}
--- src/projects/Persistence/Contexts/ApplicationDbContext.cs
+++ src/projects/Persistence/Contexts/ApplicationDbContext.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
-
+﻿using Core.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
 namespace Persistence.Contexts;
-public class ApplicationDbContext : DbContext
-{
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
+public sealed class ApplicationDbContext : ProjectDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration configuration) : base(options,configuration){ }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
     public DbSet<ProgrammingLanguage> ProgrammingLanguages { get; set; }
     public DbSet<Technology> Technologies { get; set; }
