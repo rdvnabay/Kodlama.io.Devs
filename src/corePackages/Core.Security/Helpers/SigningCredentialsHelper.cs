25bb3e92447daa66ea84610729f73d1d03c007df
Ridvan <ridvanabay@gmail.com>
2022-09-09 22:33:51 +0300

    Core Security | add Hashing, SecurityKey and SigningCredentials helper

---
+++ src/corePackages/Core.Security/Helpers/SigningCredentialsHelper.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.IdentityModel.Tokens;
+namespace Core.Security.Helpers;
+
+public class SigningCredentialsHelper
+{
+    public static SigningCredentials CreateSigningCredentials(SecurityKey securityKey) 
+        => new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512Signature);
+}
