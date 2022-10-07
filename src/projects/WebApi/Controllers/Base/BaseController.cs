965719a5c85ab1cb1d6ef17aa6bcc89bea31493f
Ridvan <ridvanabay@gmail.com>
2022-10-07 19:57:50 +0300

    #7 WebApi: add GetIpAddress method to BaseController

--- src/projects/WebApi/Controllers/Base/BaseController.cs
+++ src/projects/WebApi/Controllers/Base/BaseController.cs
@@ -7,4 +7,10 @@ public class BaseController : ControllerBase
     private IMediator _mediator;
 
     protected IMediator? Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<IMediator>();
+
+    protected string? GetIpAddress()
+    {
+        if (Request.Headers.ContainsKey("X-Forwarded-For")) return Request.Headers["X-Forwarded-For"];
+        return HttpContext.Connection.RemoteIpAddress?.MapToIPv4().ToString();
+    }
 }
